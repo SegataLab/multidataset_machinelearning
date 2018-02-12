3a5ee0235d57ebbb20562067dbdca46e053a24fd
paolo.manghi <paolo.manghi@studio.unibo.it>
2018-02-12 20:35:57 +0100

    codes for analyses based on CM tools and metaphaln2 ? humann2 profile data types

--- cmdpy_betadiversity.py
+++ cmdpy_betadiversity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 #  USAGE: 
 #  python cmdpy_dataset.py CM_periimplantitis.study_condition:peri-implantitis,col:color:crimson,col:shape:o,col:define:periimplantitis CM_periimplantitis.study_condition:control,col:color:blue,col:shape:o,col:define:control --shrink | python cmdpy_betadiversity.py -a mds -sc coords.txt -of pi_plot 
+
 
 import argparse as ap
 import sys, os
@@ -15,67 +17,81 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
+
 class beta_diversity(object):
-    def __init__(self):
-        self.args = self.read_params(sys.argv)
+
+    def __init__(self, params=None):
+
+        if not isinstance(params, dict):  self.args = self.read_params(sys.argv)
+
+        else: self.args = params
+
         self.stdin = pd.read_csv(self.args['stdin'], sep='\t', header=None, index_col=0)
+ 
         self.shapes_types = '.,ov^<>1234sp*hH+xDd|_'
         self.coordinates, self.metadata, self.explained_var = self.beta_div()
         self.sample_in_class, self.atts_of_sample, self.sample_to_class = self.guess_classes(self.metadata)
         self.legend = [ll.split() for ll in list(set([' '.join(l) for l in self.atts_of_sample.values()]))] 
-        print self.legend
 
         self.sample_and_coordinates = self.stack_sample_coordinates() \
-	if not self.args['load_coordinates'] \
-	else pd.read_csv(self.args['load_coordinates'], sep='\t', header=0, index_col=0)        
 
-        print ' YAOOOO'
-        print self.sample_and_coordinates, ' sono io sono io sono io'
+	    if not self.args['load_coordinates'] \
 
-        if self.args['save_coordinates']: 
-            cwd = os.getcwd()
-            print 'la current: ', cwd
-            self.save_coordinates(self.args['save_coordinates'])
+	    else pd.read_csv(self.args['load_coordinates'], sep='\t', header=0, index_col=0)        
+
+        if self.args['save_coordinates']: self.save_coordinates(self.args['save_coordinates'])
 
         if self.args['mask']: self.mask()
+
         if self.args['explain']: self.explain()
-        ##print not (not self.args['no_ordination'])
+
         if not (not self.args['no_ordination']): self.scatter_plot()
+
         if self.args['boxplot']: print 'mia nonna'         
+
+
 
     def read_params(self, args):
         p = ap.ArgumentParser()
         distances = ['lbraycurtis','sbraycurtis','canberra','chebyshev','correlation','dice','hamming','jaccard','kulsinski'\
 	    ,'mahalanobis','matching','minkowski','rogerstanimoto','russellrao','seuclidean','sokalmichener','sokalsneath'\
 	    ,'sqeuclidean','yule','cityblock','cosine','euclidean','l1','l2','manhattan','braycurtis','precomputed'] 
+
         arg = p.add_argument
-        arg('-if','--stdin',default=sys.stdin,type=str)
-        arg('-of','--stdout',default=None,type=str)
-        arg('-sc','--save_coordinates',default=None,type=str)
-        arg('-lc','--load_coordinates',default=None,type=str)
-        arg('-d','--distance',default='braycurtis',type=str,choices=distances)
-        arg('-a','--algorithm',default='mds',type=str,choices=['mds','pca','nmds','boxp'])
-        arg('-z','--feature_identifier',default='s__',type=str,choices=['k__','s__','PWY','UniRef90'])
-        arg('-si','--sample_id',default='sampleID',type=str)
-        arg('-ci','--classes_id',default='define',type=str)
-        arg('-m','--mask',default=[],nargs='+',type=str)
+        arg(	'-if', '--stdin', default=sys.stdin, type=str)
+        arg(	'-of', '--stdout', default=None, type=str)
+        arg(	'-sc', '--save_coordinates', default=None, type=str)
+        arg(	'-lc', '--load_coordinates', default=None, type=str)
+        arg(	'-d', '--distance', default='braycurtis', type=str, choices=distances)
+        arg(	'-a', '--algorithm', default='mds', type=str, choices=['mds','pca','nmds','boxp'])
+        arg(	'-z', '--feature_identifier', default='s__', type=str, choices=['k__','s__','PWY','UniRef90'])
+        arg(	'-si', '--sample_id', default='sampleID', type=str)
+        arg(	'-ci', '--classes_id', default='define', type=str)
+        arg(	'-m', '--mask', default=[], nargs='+', type=str)
 
-        arg('-ll','--legend_loc',default='lower center',type=str)
-        arg('-fmt','--format',default='png',type=str)
+        arg(	'-ll', '--legend_loc', default='lower center', type=str)
+        arg(	'-fmt', '--format', default='png', type=str, choices=['svg','png'])
 
-        arg('--no_ordination',action='store_false')
-        arg('--boxplot',action='store_true')
-        arg('-ex','--explain',action='store_true')
+        arg(	'--no_ordination', action='store_false')
+        arg(	'--boxplot', action='store_true')
+        arg(	'-ex', '--explain', action='store_true')
+
         return vars(p.parse_args())
+
+
 
     def explain(self):
         print ' sample_in_class: a dict with classes as keys and samples for any keys listed as values.' 
         print ' atts_of_sample: a dict with samples as keys and [class, color, shape] as values.'
         print ' sample_to_class: a dict with samples as keys and class for each as value.'
 
+
+
     def pca(self, f):
         pca = decomposition.PCA(n_components=2)
         return pca.fit_transform(f), pca.explained_variance_ratio_
+
+
 
     def mds(self, d):
         try:
@@ -85,9 +101,12 @@ class beta_diversity(object):
             print 'You Have NaNs in the data: here the coordinates'
             exit(1)     
 
+
+
     def nmds(self, d):
         nmds = manifold.MDS(n_components=2, metric=False, max_iter=3000, eps=1e-12, dissimilarity="precomputed", n_jobs=1, n_init=1)
         return nmds.fit_transform(d)
+
 
     def compute_distances(self, data, metric):
         if metric == 'precomputed': return data
@@ -100,11 +119,14 @@ class beta_diversity(object):
         else:
             return pairwise_distances(data, metric='braycurtis')
 
+
     def load_input(self, stdin): return pd.read_csv(stdin, sep='\t', header=None, index_col=0)
+
 
     def edit_(self, f, sid, feat_id):
         feats = [i for i in f.index.tolist() for fii in feat_id.split(':') if fii in i]
         return f.loc[[sid,self.args['classes_id'],'color','shape']+feats, :]
+
 
     def guess_classes(self, mdf):
         class_to_samples = dict([(c, mdf.loc[self.args['sample_id'], mdf.loc[self.args['classes_id']].isin([c])].tolist()) for c in mdf.loc[self.args['classes_id'], :].tolist()])
@@ -112,6 +134,7 @@ class beta_diversity(object):
 	    for s in mdf.loc[self.args['sample_id'], :].tolist()])
         sample_to_class = dict([(s,c) for s,c in zip(mdf.loc[self.args['sample_id'], :].tolist(), mdf.loc[self.args['classes_id'], :].tolist())])
         return class_to_samples, sample_to_attributes, sample_to_class            
+
 
     def transformation(self, way, f, feat_id, distance):
         data = f.loc[[i for i in f.index.tolist() if (feat_id in i)]].T
@@ -121,10 +144,12 @@ class beta_diversity(object):
         else: transformed, exp_var = T_func(self.compute_distances(data,distance)), None
         return transformed, metadata, exp_var
 
+
     def beta_div(self):
         edt = self.edit_(self.stdin, self.args['sample_id'], self.args['feature_identifier'])
         transform, metadata, exp_var = self.transformation(self.args['algorithm'], edt, self.args['feature_identifier'], self.args['distance'])
         return transform, metadata, exp_var
+
 
     def stack_sample_coordinates(self):
         toreturn = pd.DataFrame({self.args['sample_id']: self.metadata.loc[self.args['sample_id'], :].tolist(), 'x1': self.coordinates[:, 0], 'x2': self.coordinates[:, 1]})
@@ -132,26 +157,41 @@ class beta_diversity(object):
         del toreturn[self.args['sample_id']]
         return toreturn
 
-    def save_coordinates(self, coordinates_f): 
-        print 'sto salvando le coordinate_ '
-        self.sample_and_coordinates.to_csv(coordinates_f, sep='\t', header=True, index=True)
+
+    def save_coordinates(self, coordinates_f): self.sample_and_coordinates.to_csv(coordinates_f, sep='\t', header=True, index=True)
+
     
     def mask(self):
         self.sample_and_coordinates = self.sample_and_coordinates.drop([i for i in self.sample_and_coordinates.index.tolist() if self.sample_to_class[i] in self.args['mask']])
         for i in self.sample_to_class.keys(): 
             if self.sample_to_class[i] in self.args['mask']: del self.atts_of_sample[i]        
 
-    def scatter_plot(self):
-        fig, ax = plt.subplots(figsize=(10,10))
+
+    def scatter_plot(self, ax=None):
+
+        fig = False
+
+        if not bool(ax):
+            fig, ax = plt.subplots(figsize=(10,10))
+
         for c in self.sample_in_class.keys():
+
             present = [s for s in self.sample_in_class[c] if s in set(self.sample_and_coordinates.index.tolist())]
             present_sample_frame = self.sample_and_coordinates.loc[present]
-            scatter = sns.regplot(x='x1', y='x2', data=present_sample_frame, ax=ax, scatter=True, fit_reg=False, ##scatter_kws={'s':]}\
+
+            scatterp = sns.regplot(x='x1', y='x2', data=present_sample_frame, ax=ax, scatter=True, fit_reg=False, ax=ax\
 		, label=self.atts_of_sample[present[0]][0], marker=self.atts_of_sample[present[0]][2], color=self.atts_of_sample[present[0]][1])
-        ax.legend(loc=self.args['legend_loc'])
-        plt.savefig(self.args['stdout']+'.'+self.args['format'], dpi=300) 
+
+        if bool(fig):
+            ax.legend(loc=self.args['legend_loc'])
+            plt.savefig(self.args['stdout']+'.'+self.args['format'], dpi=400) 
+
+        else:
+            return scatterp
+
 
 if __name__ == '__main__':
+
     bd = beta_diversity()
      
     ## print bd.sample_and_coordinates
--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@
 # grid0, grid1, grid2 are terms present in the various file names
 # for parameters to be tuned.
 
+
 class usefullfuncs(object):
+
     data_aliases = {'CM_rescignocrc':'CM_Cohort2', 'CM_lilt':'CM_Cohort1'}
     features = dict([(data,feat) for data,feat in zip(\
 		['metaphlan','pfam','metaphlan_pfam','genefamilies','pathways']\
@@ -60,7 +62,7 @@ class usefullfuncs(object):
         return '../ml/resultoftransfer_*'+'_ON_'+dataset_on_which+'_features:'+('_'.join(database) if isinstance(database, list) else database)\
 	      +'_experimenttype:standard_'+algo+'_grid0:'+grid0+'_grid1:'+grid1+'_grid2:'+grid2+'.txt'
 
-        ## ml/cross_validation_ANYonANY_features:pathways_experimenttype:standard_rf_grid:gini_featsel:auto.txt
+        ### ml/cross_validation_ANYonANY_features:pathways_experimenttype:standard_rf_grid:gini_featsel:auto.txt
 
     def transfer_(self, two_datasets, db, test, algo, grid0, grid1, grid2, start, changed_coordinates=[]):
         score, n_samples = self._auc(self.get_transfer_resultnames(two_datasets, db, test, algo, grid0, grid1, grid2), start)
@@ -91,6 +93,61 @@ class usefullfuncs(object):
 
     def test_magns(self, db, test, algo, grid0, grid1, grid2, start):
         return dict([(source, self._auc(self.get_transfer_resultnames([source, source], db, test, algo, grid0, grid1, grid2), start)[1]) for source in self.datasets])
+
+
+
+class effect_size(object): 
+
+    def __init__(self, e, n, c, p):
+        self.esize = e
+        self.name = n
+        self.cl = c
+        self.pv = p
+
+
+class lefse_reader(object):
+
+    def __init__(self, lefse_output):
+         self.to_read = lefse_output
+         self.data = list()
+         self.classes_ = set()
+
+
+    def select_highest(self, howmany=5):
+        feats = list()
+        for c in self.classes_:
+            feats += [feat[1] for feat in sorted([(d.esize, d.name) for d in self.data if d.cl==c]\
+		, key = lambda obj : obj[0], reverse=True)][:howmany]
+   
+            #print ' inside: ', feats
+            #exit(1)
+
+        self.feats = feats
+        self.data = [d for d in self.data if d.name in feats]
+
+       
+    def get(self):
+        line = True
+        with open(self.to_read) as ip:
+            while line:
+                line = ip.readline().rstrip().split()
+                if len(line) == 5:
+                    feat = str(line[0]) if not line[0].startswith('s__') else str(line[0][3:])
+                    class_ = str(line[2])
+                    pvalue = float(line[-1])
+                    e_size = float(line[1])
+                    self.data.append(effect_size(e_size, feat, class_, pvalue))
+                    self.classes_.add(class_)
+
+
+class project_color_code(object):
+
+    def __init__(self, project_name):
+
+        if project_name == 'periimplantitis': self.color_code = dict([(cat,cl) for cat,cl in zip(['healthy', 'peri-implantitis', 'mucositis'], ['seagreen', 'dodgerblue', 'orangered'])])
+
+
+
 
 if __name__ == '__main__':
     print 'morditi il culo!, for parameters to be tuned. passa parola'
